facee1210da7381e64d4282c0209d7a641dc852c
大窪泰一 <taichi@Taichi-2.local>
2025-12-16 05:32:42 +0900

    add:flash_typesの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :info, :success, :warning, :error
+
   protected
 
   def configure_permitted_parameters

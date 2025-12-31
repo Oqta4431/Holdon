24c5cf7af0042ab84d6b54d4f582236b8107e0a7
大窪泰一 <taichi@Taichi-2.local>
2026-01-01 07:42:30 +0900

    change:ReminderモデルとItemモデルのリレーションを追記

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,2 +1,6 @@
 class Reminder < ApplicationRecord
+  validates :remind_at, presence: true
+  validates :remind_interval, numericality: { greater_than: 0 }, allow_nil: true
+
+  belongs_to :item
 end

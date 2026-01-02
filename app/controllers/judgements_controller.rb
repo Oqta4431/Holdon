dd9c84b1e124aac06c1d26da1507a14832121440
大窪泰一 <taichi@Taichi-2.local>
2026-01-02 14:21:32 +0900

    change:購入判断時のリマインド間隔の変更を追記

--- app/controllers/judgements_controller.rb
+++ app/controllers/judgements_controller.rb
@@ -4,12 +4,19 @@ class JudgementsController < ApplicationController
   def update
     item = current_user.items.find(params[:item_id])
     judgement = item.judgement
+    status = judgement_params[:purchase_status]
 
-    judgement.update!(
-      purchase_status: judgement_params[:purchase_status],
-      decided_at: Time.current
-    )
+    ## 購入判断の更新、判断時刻の更新、検討中の場合のリマインド時刻の更新を3つセットで行う
+    Judgement.transaction do
+      judgement.update!(purchase_status: status, decided_at: Time.current)
 
+      if status == "considering"
+        item.reminder.update!(remind_at: Time.current + REMIND_INTERVAL)
+      end
+      ## purchased / skipped の場合はリマインド時刻は更新しない
+    end
+
+    ## ⚠️購入判断画面を作成したらリダイレクト先を変更すること⚠️
     redirect_to item_path(item), notice: "購入判断を更新しました"
   end
 

a4a23e03b93294e936bc214a8cc115b39ae32c5c
大窪泰一 <taichi@Taichi-2.local>
2026-03-17 23:21:37 +0900

    fix:rubocop violation の修正

--- db/migrate/20260307051016_create_categories.rb
+++ db/migrate/20260307051016_create_categories.rb
@@ -8,6 +8,6 @@ class CreateCategories < ActiveRecord::Migration[7.2]
 
     # 同一ユーザー内でカテゴリー名が重複しないようにユニーク制約を追加
     # DBレベルで保証することでモデルバリデーションをすり抜けた場合も防止できる
-    add_index :categories, [:user_id, :name], unique: true
+    add_index :categories, [ :user_id, :name ], unique: true
   end
 end

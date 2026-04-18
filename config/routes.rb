26ccfd34e282f6cf00553d08e7e832e0991131e5
大窪泰一 <taichi@Taichi-2.local>
2026-04-18 14:28:10 +0900

    change:rootを仮ページからhomeに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "home/index"
-  # root "home#index"
-  get "posts/index"
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,9 +10,6 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  root "posts#index"
 
   resources :items do
     resource :judgement, only: %i[update]

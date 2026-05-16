c3b0189357ceedfc80f0eaa064b0f2b19a5d9994
大窪泰一 <taichi@Taichi-2.local>
2026-05-16 14:36:24 +0900

    fix: fing_or_create_from_omniauth で authがnilの場合のエラーハンドリングの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,17 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
 
   def self.find_or_create_from_omniauth(auth)
-    if auth.present?
-      prof = User.find_or_initialize_by(provider: auth["provider"], uid: auth["uid"])
-      if prof.new_record?
-          # deviseの挙動対策としてフェイクアドレスを作成
-          email = auth["info"]["email"] ? auth["info"]["email"] : "#{auth["uid"]}-#{auth["provider"]}@example.com"
-          prof.email = email
-          prof.name =  auth["info"]["name"]
-          prof.password = Devise.friendly_token[0, 20]
-          prof.save!
-      end
-      prof
+    raise ArgumentError, "auth is blank" if auth.blank?
+
+    prof = User.find_or_initialize_by(provider: auth["provider"], uid: auth["uid"])
+    if prof.new_record?
+        # deviseの挙動対策としてフェイクアドレスを作成
+        email = auth["info"]["email"] ? auth["info"]["email"] : "#{auth["uid"]}-#{auth["provider"]}@example.com"
+        prof.email = email
+        prof.name =  auth["info"]["name"]
+        prof.password = Devise.friendly_token[0, 20]
+        prof.save!
     end
+    prof
   end
 end

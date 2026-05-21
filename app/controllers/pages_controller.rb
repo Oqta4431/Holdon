670d5caaeac8821da4f4f90a14fb916f4a20db43
大窪泰一 <taichi@Taichi-2.local>
2026-05-21 21:38:50 +0900

    fix: rubocop violationsの修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
-  layout -> { user_signed_in? ? 'application' : 'landing' }, only: [:terms, :privacy]
+  layout -> { user_signed_in? ? "application" : "landing" }, only: [ :terms, :privacy ]
 
   def onboarding
     return redirect_to home_path if user_signed_in?
 
-    render layout: 'landing'
+    render layout: "landing"
   end
 
   def terms

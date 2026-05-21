c3a0064f50ee43b87b809084e46affa1e44224ac
大窪泰一 <taichi@Taichi-2.local>
2026-05-21 21:55:59 +0900

    fix: OGPスペックのsign_inを削除しonboardingのリダイレクトテストを追加

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.describe "Pages", type: :request do
+  describe "GET /" do
+    context "未ログイン" do
+      it "200 OK で閲覧できる" do
+        get root_path
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "ログイン済み" do
+      let(:user) { create(:user) }
+
+      before { sign_in user }
+
+      it "home_path にリダイレクトされる" do
+        get root_path
+
+        expect(response).to redirect_to(home_path)
+      end
+    end
+  end
+
   describe "GET /terms" do
     context "未ログイン" do
       it "200 OK で閲覧できる" do

5b48a64fd927ecc96551cd8bc528285c08010aab
大窪泰一 <taichi@Taichi-2.local>
2026-05-21 21:35:47 +0900

    add: オンボーディング画面とlandingレイアウトを実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  layout -> { user_signed_in? ? 'application' : 'landing' }, only: [:terms, :privacy]
+
+  def onboarding
+    return redirect_to home_path if user_signed_in?
+
+    render layout: 'landing'
+  end
+
   def terms
   end
 
